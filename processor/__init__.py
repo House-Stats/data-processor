e2992ead047a790b2453735dfd7d3c1e197c18f5
Morgan Thomas <morganrhysthoams14@gmail.com>
2023-01-30 12:34:12 +0000

    Added sentry logging

--- processor/__init__.py
+++ processor/__init__.py
@@ -1,6 +1,11 @@
 from main import Processor
+import sentry_sdk
 
 if __name__ == "__main__":
+    sentry_sdk.init(
+        dsn="https://a965c86590964e07b4b759d66633bb62@sentry.housestats.co.uk/3",
+        traces_sample_rate=1.0
+    )
     print("loading")
     processor = Processor()
     print("Running")
